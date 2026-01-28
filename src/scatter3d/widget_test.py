49591efcd6825f378462637dceff12bb8352d4bd
Jose Blanca <jblanca@upv.es>
2026-01-28 10:09:07 +0100

    remove broken code

--- src/scatter3d/widget_test.py
+++ src/scatter3d/widget_test.py
@@ -19,12 +19,27 @@ def _():
     species_list = ["species1", "species2", "species3"]
     species = random.choices(species_list, k=num_points)
     species = Category(pandas.Series(species, name="species"))
+    countries_list = ["country1", "country2", "country3"]
+    countries = random.choices(countries_list, k=num_points)
+    countries = Category(pandas.Series(countries, name="countries"))
 
     w = Scatter3dWidget(xyz=points, category=species, point_ids=point_ids)
     w.height = 800
     ui = marimo.ui.anywidget(w)
-    ui
     return species, ui, w
+
+
+@app.cell
+def _(species):
+    category = species
+    return (category,)
+
+
+@app.cell
+def _(category, ui, w):
+    w.category = category
+    ui
+    return
 
 
 @app.cell
@@ -34,14 +49,6 @@ def _(species, ui, w):
     print(species.num_unassigned)
     print(species.values)
     print(w.point_ids)
-    return
-
-
-@app.cell
-def _(w):
-    import inspect
-    print(type(w))
-    print(inspect.getsource(type(w)._on_lasso_request_t))
     return
 
 
